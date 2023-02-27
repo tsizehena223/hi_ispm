197106dc334c73a373e1c7afdeb7a072ea342681
Tsizehena <tsizehena223@gmail.com>
2023-02-27 13:38:09 +0300

    Ajout de lien

--- cavio/src/controllers/cavioFunctions.php
+++ cavio/src/controllers/cavioFunctions.php
@@ -1,11 +1,12 @@
 <?php
-function result($arr){
+function result($arr)
+{
     $res = getResult($arr);
     $i = 1;
     $resBest = [];
-    foreach($res as $k => $n){
+    foreach ($res as $k => $n) {
         $resBest[] = [$k, $n];
-        if($i == 2){
+        if ($i == 2) {
             break;
         }
         $i++;
@@ -13,7 +14,8 @@ function result($arr){
     return $resBest;
 }
 
-function getResult($arr){
+function getResult($arr)
+{
 
     $inv = compteur('I', $arr);
     $rea = compteur('R', $arr);
@@ -23,40 +25,45 @@ function getResult($arr){
     $conv = compteur('V', $arr);
 
     $res = [
-        'INVERTIGATEUR' => $inv, 
-        'REALISTE' => $rea, 
-        'ARTISTIQUE' => $art, 
-        'SOCIAL' => $soci, 
-        'ENTREPRENANT' => $ent, 
+        'INVERTIGATEUR' => $inv,
+        'REALISTE' => $rea,
+        'ARTISTIQUE' => $art,
+        'SOCIAL' => $soci,
+        'ENTREPRENANT' => $ent,
         'CONVENITONNEL' => $conv
     ];
     arsort($res);
     return $res;
 }
 
-function compteur(String $s, array $arr){
+function compteur(String $s, array $arr)
+{
     $cpt = 0;
     $key = array_keys($arr);
     $i = 0;
-    foreach($arr as $n){
+    foreach ($arr as $n) {
         $key = array_keys($arr);
         $key[$i][0];
-        if($s == $key[$i][0]){
+        if ($s == $key[$i][0]) {
             $cpt += (int)$n;
         }
         $i++;
     }
     return $cpt;
 }
-function format_result($resBest){
+function format_result($resBest)
+{
     $best1 = $resBest[0][0];
     $best2 = $resBest[1][0];
+
+    $best1_points = $resBest[0][1];
+    $best2_points = $resBest[1][1];
 
     // Generate Url
     $lien1 = strtolower($best1);
     $lien2 = strtolower($best2);
     $lien1 = "https://www.123test.com/fr/la-personnalite-de-type-$lien1/";
     $lien2 = "https://www.123test.com/fr/la-personnalite-de-type-$lien2/";
-    echo "<a href=\"$lien1\">$best1</a><br>";
-    echo "<a href=\"$lien2\">$best2</a>";
+    echo "<a href=\"$lien1\" target=\"blank\">$best1 : $best1_points</a><br>";
+    echo "<a href=\"$lien2\" target=\"blank\">$best2 : $best2_points</a>";
 }
