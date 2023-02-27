841ab5b370964cc3f5ab2b98677fb43919ae50c8
athanase25537 <athanase21.aps2a@gmail.com>
2023-02-27 14:53:44 +0300

    Add Definitions

--- cavio/src/controllers/cavioFunctions.php
+++ cavio/src/controllers/cavioFunctions.php
@@ -1,12 +1,11 @@
 <?php
-function result($arr)
-{
+function result($arr){
     $res = getResult($arr);
     $i = 1;
     $resBest = [];
-    foreach ($res as $k => $n) {
+    foreach($res as $k => $n){
         $resBest[] = [$k, $n];
-        if ($i == 2) {
+        if($i == 2){
             break;
         }
         $i++;
@@ -14,8 +13,7 @@ function result($arr)
     return $resBest;
 }
 
-function getResult($arr)
-{
+function getResult($arr){
 
     $inv = compteur('I', $arr);
     $rea = compteur('R', $arr);
@@ -25,45 +23,67 @@ function getResult($arr)
     $conv = compteur('V', $arr);
 
     $res = [
-        'INVERTIGATEUR' => $inv,
-        'REALISTE' => $rea,
-        'ARTISTIQUE' => $art,
-        'SOCIAL' => $soci,
-        'ENTREPRENANT' => $ent,
+        'INVESTIGATEUR' => $inv, 
+        'REALISTE' => $rea, 
+        'ARTISTIQUE' => $art, 
+        'SOCIAL' => $soci, 
+        'ENTREPRENANT' => $ent, 
         'CONVENITONNEL' => $conv
     ];
     arsort($res);
     return $res;
 }
 
-function compteur(String $s, array $arr)
-{
+function compteur(String $s, array $arr){
     $cpt = 0;
     $key = array_keys($arr);
     $i = 0;
-    foreach ($arr as $n) {
+    foreach($arr as $n){
         $key = array_keys($arr);
         $key[$i][0];
-        if ($s == $key[$i][0]) {
+        if($s == $key[$i][0]){
             $cpt += (int)$n;
         }
         $i++;
     }
     return $cpt;
 }
-function format_result($resBest)
-{
+function format_result($resBest){
     $best1 = $resBest[0][0];
     $best2 = $resBest[1][0];
-
-    $best1_points = $resBest[0][1];
-    $best2_points = $resBest[1][1];
 
     // Generate Url
     $lien1 = strtolower($best1);
     $lien2 = strtolower($best2);
     $lien1 = "https://www.123test.com/fr/la-personnalite-de-type-$lien1/";
     $lien2 = "https://www.123test.com/fr/la-personnalite-de-type-$lien2/";
-    echo "<a href=\"$lien1\" target=\"blank\">$best1 : $best1_points</a><br>";
-    echo "<a href=\"$lien2\" target=\"blank\">$best2 : $best2_points</a>";
+
+    // Description
+    $description1 = getDescription($best1);
+    $description2 = getDescription($best2);
+
+    echo "<h1>$best1</h1>
+        <p>$description1<a href=\"$lien1\">En savoir plus</a></p>";
+    echo "<h1>$best2</h1>
+        <p>$description2<a href=\"$lien2\">En savoir plus</a></p>";
+}
+
+function getDescription($mot){
+    $definions = [
+        'INVESTIGATEUR' => "La personnalité de type Investigateur a tendance à être portée sur l’analyse, l’intellect et l’érudition. Cette personne aime la recherche, les activités mathématiques ou scientifiques. Elle a tendance à vivre dans ses pensées et préfère aborder le monde réel à distance. Elle aime lire, étudier, utiliser des livres et d’autres données au lieu de travailler avec ses mains.", 
+        'REALISTE' => "La personnalité de type Réaliste aime utiliser ses mains et ses yeux pour explorer le monde et accomplir des choses. Cette personne aime les activités et occupations extérieures, mécaniques et physiques.", 
+        'ARTISTIQUE' => "La personnalité de type Artistique utilise ses mains et son esprit pour créer de nouvelles choses. Cette personne apprécie la beauté, les activités non structurées et la variété. Elle aime les personnes, vues, textures et sons intéressants et inhabituels.", 
+        'SOCIAL' => "Les personnes de type Social sont des leaders dévoués, humanistes, responsables et solidaires. Elles utilisent les sentiments, les mots et les idées pour travailler avec les gens plutôt que l’activité physique pour effectuer des tâches.", 
+        'ENTREPRENANT' => "Qu’est-ce qu’une personne entreprenante ? Elle aime travailler avec les gens, influencer, persuader, diriger ou gérer pour des objectifs organisationnels ou des gains économiques. La personnalité de type Entreprenant est souvent un leader qui a du talent pour organiser, persuader et gérer.", 
+        'CONVENITONNEL' => "La personnalité de type Conventionnel aime travailler avec des données et des chiffres, exécuter des tâches en détail et suivre les instructions données par d’autres. Il est calme, prudent, responsable, bien organisé et concentré sur ses tâches"
+    ];
+
+    $description = '';
+    foreach($definions as $k => $definition){
+        if($mot == $k){
+            $description = $definition;
+            break;
+        }
+    }
+    return $description;
 }
---
+++ cavio/src/controllers/definitions.php
@@ -0,0 +1,9 @@
+<?php
+$definions = [
+    'INVERTIGATEUR' => "La personnalité de type Investigateur a tendance à être portée sur l’analyse, l’intellect et l’érudition. Cette personne aime la recherche, les activités mathématiques ou scientifiques. Elle a tendance à vivre dans ses pensées et préfère aborder le monde réel à distance. Elle aime lire, étudier, utiliser des livres et d’autres données au lieu de travailler avec ses mains.", 
+    'REALISTE' => "La personnalité de type Réaliste aime utiliser ses mains et ses yeux pour explorer le monde et accomplir des choses. Cette personne aime les activités et occupations extérieures, mécaniques et physiques.", 
+    'ARTISTIQUE' => "La personnalité de type Artistique utilise ses mains et son esprit pour créer de nouvelles choses. Cette personne apprécie la beauté, les activités non structurées et la variété. Elle aime les personnes, vues, textures et sons intéressants et inhabituels.", 
+    'SOCIAL' => "Les personnes de type Social sont des leaders dévoués, humanistes, responsables et solidaires. Elles utilisent les sentiments, les mots et les idées pour travailler avec les gens plutôt que l’activité physique pour effectuer des tâches.", 
+    'ENTREPRENANT' => "Qu’est-ce qu’une personne entreprenante ? Elle aime travailler avec les gens, influencer, persuader, diriger ou gérer pour des objectifs organisationnels ou des gains économiques. La personnalité de type Entreprenant est souvent un leader qui a du talent pour organiser, persuader et gérer.", 
+    'CONVENITONNEL' => "La personnalité de type Conventionnel aime travailler avec des données et des chiffres, exécuter des tâches en détail et suivre les instructions données par d’autres. Il est calme, prudent, responsable, bien organisé et concentré sur ses tâches"
+];
