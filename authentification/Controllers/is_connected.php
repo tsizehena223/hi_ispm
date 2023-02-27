95b9505ae11bbd5142a50776244cedb0ceb47894
Tsizehena <tsizehena223@gmail.com>
2023-02-27 13:57:34 +0300

    Oups

--- authentification/Controllers/is_connected.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!isset($_SESSION['auth'])) {
-    $_SESSION['flash']['error'] = "Vous n'êtes pas encore connecté!";
-?>
-    <script>
-        location.replace("./connexion_view.php");
-    </script>
-<?php }
