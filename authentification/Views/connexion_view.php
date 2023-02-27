dbb39e47e77e0c1cb732786587833f4e20314aa6
Tsizehena <tsizehena223@gmail.com>
2023-02-27 13:10:45 +0300

    Design de formulaire

--- authentification/Views/connexion_view.php
+++ authentification/Views/connexion_view.php
@@ -5,47 +5,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/connexion.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <title>Connexion</title>
 </head>
 
 <body>
-    <div class="signup-box">
-        <h1>Connexion</h1>
-        <center>
-            <h2 style="color: red">
+    <center>
+        <div class="signin-box">
+            <h1>Connexion</h1>
+            <center>
                 <?php
                 if (session_status() == PHP_SESSION_NONE) {
                     session_start();
                 }
-                if (isset($_SESSION['flash']['error'])) {
-                    echo $_SESSION['flash']['error'];
-                }
-                unset($_SESSION['flash']['error']);
-                ?>
-            </h2>
-            <h2 style="color: green">
+                if (isset($_SESSION['flash']['error'])) :  ?>
+                    <div class="alert alert-danger col-md-4">
+                        <?= $_SESSION['flash']['error'] ?>
+                    </div>
                 <?php
-                if (session_status() == PHP_SESSION_NONE) {
-                    session_start();
-                }
-                if (isset($_SESSION['flash']['success'])) {
-                    echo $_SESSION['flash']['success'];
-                }
-                unset($_SESSION['flash']['success']);
+                    unset($_SESSION['flash']['error']);
+                endif;
                 ?>
-            </h2>
-        </center>
-        <form action="../Controllers/connexionController.php" method="post">
-            <label>Pseudo : </label>
-            <input type="text" placeholder="Votre nom" name="name" required /> <br><br>
-            <label>Password</label>
-            <input type="password" placeholder="Mot de passe" name="mdp" required /> <br><br>
-            <button type="submit" name="connexion">Se connecter</button>
-        </form>
-    </div>
-    <p class="para-2">
-        <a href="inscription_view.php">S'inscrire ici</a>
-    </p>
+                <?php
+                if (isset($_SESSION['flash']['success'])) :  ?>
+                    <div class="alert alert-success col-md-4">
+                        <?= $_SESSION['flash']['success'] ?>
+                    </div>
+                <?php
+                    unset($_SESSION['flash']['success']);
+                endif;
+                ?>
+            </center>
+            <form action="../Controllers/connexionController.php" method="post">
+                <div class="col-md-4">
+                    <label>Pseudo : </label>
+                    <input type="text" placeholder="Votre nom" name="name" required class="form-control" /> <br><br>
+                </div>
+                <div class="col-md-4">
+                    <label>Mot de passe : </label>
+                    <input type="password" placeholder="Mot de passe" name="mdp" required class="form-control" /> <br><br>
+                </div>
+                <div class="row col-md-3">
+                    <button type="submit" name="connexion" class="btn btn-danger">Se connecter</button> &nbsp;
+                    <a href="inscription_view.php" class="btn btn-outline-info">S'inscrire</a>
+                </div>
+            </form>
+        </div>
+    </center>
 </body>
 
 </html>
